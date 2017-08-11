d3267d2bd4dec056cd63c0385a6084f0de3de7d8
Manoj <manoj.vattarai@gmail.com>
2017-08-11 23:32:49 +0545

    Changed on admin of blog site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 3
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['author','title','text']}),
+        ('Date information', {'fields': ['created_date','published_date']}),
+    ]
+    inlines = [CommentInline]
+    list_display = ('author', 'title','text', 'created_date','published_date')
+    list_filter = ['created_date','published_date']
+    search_fields = ['title','text']
+    list_per_page = 3
+
+
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
